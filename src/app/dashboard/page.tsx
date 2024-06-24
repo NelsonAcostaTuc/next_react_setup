a189cdf8e07df0e6ba24f4da7017cccd5f91b8e8
alexmorales33 <alexmoraleess4@gmail.com>
2024-06-24 13:56:42 -0300

    update eslint and prettier

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,19 @@
+'use client';
+import { getAllUsers } from '@/redux/actions/userActions';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { useEffect } from 'react';
 
 export default function Page() {
+  const dispatch = useAppDispatch();
+  const { allUsers, loading, error } = useAppSelector((state) => state.user);
+
   useEffect(() => {
-    console.log('Hola mundo');
-  }, []);
+    dispatch(getAllUsers());
+  }, [dispatch]);
+
+  useEffect(() => {
+    console.log(allUsers);
+  }, [allUsers]);
+
   return <h1>Hello, Dashboard Page!</h1>;
 }
