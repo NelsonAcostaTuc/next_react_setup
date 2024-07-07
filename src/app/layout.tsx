2f5c1bd1235dc791c82f557012dbf9795427b7b4
Nelson Acosta <nelsongacosta@gmail.com>
2024-07-07 09:22:58 -0300

    change description app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Somnio Software',
-  description: 'Somnio Software Challenge',
+  title: 'Test Fast API',
+  description: 'Test Fast API',
 };
 
 export default function RootLayout({
